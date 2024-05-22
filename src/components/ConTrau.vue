97ab6ce72d46b6a11e828dfbb243f3d0fc46d8b9
hoang trung <th599396@gmail.com>
2024-05-23 00:53:05 +0700

    first commit

---
+++ src/components/ConTrau.vue
@@ -0,0 +1,22 @@
+<script >
+export default {
+  name: 'Trau',
+  props: {
+    gioi: String
+  }
+}
+
+
+</script>
+
+<template>
+  <div>
+    <h1>{{ gioi }}</h1>
+
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
